83c1010fdaecc2e8107ef65becd5602634ce4f47
Refun <refunfk@gmail.com>
2022-07-14 16:58:33 +0530

    first commit

---
+++ rought.py
@@ -0,0 +1,11 @@
+a=[]
+n=int(input('enter the limites:'))
+for i in range (0,n):
+	k=int(input())
+	a.append(k)
+
+for i in range (0,n):
+    if a[i]==a[i]:
+        break
+    else:
+        print(a[i])
